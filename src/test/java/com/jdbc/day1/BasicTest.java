11f10c420ef2f837520f3eaf473e979cf9a02110
Mohammed Sohrabi <najeebsohrabi@gmail.com>
2020-05-12 19:02:49 -0700

    Last

--- src/test/java/com/jdbc/day1/BasicTest.java
+++ src/test/java/com/jdbc/day1/BasicTest.java
@@ -1,8 +1,6 @@
 package com.jdbc.day1;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class BasicTest {
 
@@ -12,6 +10,44 @@ public class BasicTest {
         String password = "hr";
 
         Connection connection = DriverManager.getConnection(URL, username, password);
+        Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        ResultSet result = statement.executeQuery("SELECT * FROM employees");
 
+        while (result.next()) {
+            System.out.println("| " + result.getString(1) +" | " + result.getString(2) +
+                    " " + result.getString(3) +" | " + result.getString("salary"));
+        }
+//        result.beforeFirst();
+
+//        while (result.next()) {
+//            System.out.println(result.getString("salary"));
+//        }
+
+
+        DatabaseMetaData databaseMetaData = connection.getMetaData();
+        ResultSetMetaData resultSetMetaData = result.getMetaData();
+
+        System.out.println("JDBC Driver: "+databaseMetaData.getDriverName());
+        System.out.println("JDBC Driver version: "+databaseMetaData.getDriverVersion());
+        System.out.println("Database name: "+databaseMetaData.getDatabaseProductName());
+        System.out.println("Database version: "+databaseMetaData.getDatabaseProductVersion());
+
+        System.out.println("========================================");
+        //iterate rows
+        for (int columnIndex = 1; columnIndex <= resultSetMetaData.getColumnCount(); columnIndex++) {
+            System.out.printf("%-20s", resultSetMetaData.getColumnName(columnIndex));
+        }
+        result.beforeFirst();
+        System.out.println("");
+        while (result.next()) {
+            //iterate columns
+            for (int columnIndex = 1; columnIndex <= resultSetMetaData.getColumnCount(); columnIndex++) {
+                System.out.printf("%-20s", result.getString(columnIndex));
+            }
+            System.out.println("");
+        }
+        result.close();
+        statement.close();
+        connection.close();
     }
 }
